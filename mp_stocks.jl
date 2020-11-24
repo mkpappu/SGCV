9eca53efc529f61d11afdece46f679dfe5f44643
albertpod <albert.podusenko@gmail.com>
2020-11-24 16:46:39 +0100

    Add stocks experiments

---
+++ mp_stocks.jl
@@ -0,0 +1,153 @@
+using Revise
+using ForneyLab
+include("sgcv/SGCV.jl")
+using Main.SGCV
+using GCV
+using Plots
+using SparseArrays
+using Random
+using ProgressMeter
+include("compatibility.jl")
+
+
+pad(sym::Symbol, t::Int) = sym*:_*Symbol(lpad(t,3,'0')) # Left-pads a number with zeros, converts it to symbol and appends to sym
+
+function generate_mp_2l(ndim, n_samples)
+    fg = FactorGraph()
+    z2 = Vector{Variable}(undef, n_samples)
+    s2 = Vector{Variable}(undef, n_samples)
+    z1 = Vector{Variable}(undef, n_samples)
+    s1 = Vector{Variable}(undef, n_samples)
+    x = Vector{Variable}(undef, n_samples)
+    y = Vector{Variable}(undef, n_samples)
+    @RV A2 ~ Dirichlet(ones(ndim, ndim))
+    @RV A1 ~ Dirichlet(ones(ndim, ndim))
+    @RV [id=pad(:z1,1)] z1[1] ~ GaussianMeanPrecision(placeholder(:mz_prior1), placeholder(:wz_prior1))
+    @RV [id=pad(:z2,1)] z2[1] ~ GaussianMeanPrecision(placeholder(:mz_prior2), placeholder(:wz_prior2))
+    @RV [id=pad(:x,1)] x[1] ~ GaussianMeanPrecision(placeholder(:mx_prior1), placeholder(:wx_prior1))
+    @RV [id=pad(:y,1)] y[1] ~ GaussianMeanPrecision(x[1], placeholder(:wy_prior1))
+    @RV [id=pad(:s1,1)] s1[1] ~ ForneyLab.Categorical(ones(ndim) ./ ndim)
+    @RV [id=pad(:s2,1)] s2[1] ~ ForneyLab.Categorical(ones(ndim) ./ ndim)
+    placeholder(y[1], :y, index = 1)
+    for t in 2:n_samples
+        @RV [id=pad(:s2, t)] s2[t] ~ Transition(s2[t-1], A2)
+        @RV [id=pad(:s1, t)] s1[t] ~ Transition(s1[t-1], A1)
+        @RV [id=pad(:z2,t)] z2[t] ~ GaussianMeanPrecision(z2[t - 1], placeholder(pad(:wz_transition2, t)))
+        @RV [id=pad(:z1,t)] z1[t] ~ SwitchingGaussianControlledVariance(z1[t - 1], z2[t], ones(ndim), placeholder(pad(:ωs2, t), dims=(ndim, )),s2[t])
+        @RV [id=pad(:x,t)] x[t] ~ SwitchingGaussianControlledVariance(x[t - 1], z1[t], ones(ndim), placeholder(pad(:ωs1, t), dims=(ndim, )),s1[t])
+        @RV [id=pad(:y,t)] y[t] ~ GaussianMeanPrecision(x[t], placeholder(pad(:wy_transition, t)))
+        placeholder(y[t], :y, index = t)
+    end
+    q = PosteriorFactorization(x, z1, s1, z2, s2, A1, A2, ids=[:X :Z1 :S1 :Z2 :S2 :A1 :A2])
+    algo = messagePassingAlgorithm(free_energy=true)
+    src_code = algorithmSourceCode(algo, free_energy=true);
+    return src_code
+end
+
+function generate_mp(ndim, n_samples)
+    fg = FactorGraph()
+    z = Vector{Variable}(undef, n_samples)
+    x = Vector{Variable}(undef, n_samples)
+    y = Vector{Variable}(undef, n_samples)
+    s = Vector{Variable}(undef, n_samples)
+    @RV A ~ Dirichlet(ones(ndim, ndim))
+    @RV [id=pad(:z,1)] z[1] ~ GaussianMeanPrecision(placeholder(:mz_prior1), placeholder(:wz_prior1))
+    @RV [id=pad(:x,1)] x[1] ~ GaussianMeanPrecision(placeholder(:mx_prior1), placeholder(:wx_prior1))
+    @RV [id=pad(:y,1)] y[1] ~ GaussianMeanPrecision(x[1], placeholder(:wy_prior1))
+    @RV [id=pad(:s,1)] s[1] ~ ForneyLab.Categorical(ones(ndim) ./ ndim)
+    placeholder(y[1], :y, index = 1)
+    for t in 2:n_samples
+        @RV [id=pad(:s,t)] s[t] ~ Transition(s[t-1], A)
+        @RV [id=pad(:z,t)] z[t] ~ GaussianMeanPrecision(z[t - 1], placeholder(pad(:wz_transition, t)))
+        @RV [id=pad(:x,t)] x[t] ~ SwitchingGaussianControlledVariance(x[t - 1], z[t],ones(ndim),placeholder(pad(:ωs, t), dims=(ndim, )),s[t])
+        @RV [id=pad(:y,t)] y[t] ~ GaussianMeanPrecision(x[t], placeholder(pad(:wy_transition, t)))
+        placeholder(y[t], :y, index = t)
+    end
+    q = PosteriorFactorization(x, z ,s, A, ids=[:X :Z :S :A])
+    algo = messagePassingAlgorithm(free_energy=true)
+    src_code = algorithmSourceCode(algo, free_energy=true);
+    return src_code
+end
+
+function mp(obs;
+    ndims,
+    n_its = 50,
+    wy_prior1 = 1.0,
+    κ_m_prior = ones(ndims),
+    ω_m_prior = omegas,
+    κ_w_prior =  huge .* diageye(ndims),
+    ω_w_prior = huge .* diageye(ndims),
+    z_m_prior = 0.0,
+    z_w_prior = 100.0,
+    x_m_prior = 70.0,
+    x_w_prior = 1.0,
+    x_x_m_prior = zeros(ndims),
+    x_x_w_prior = 1.0*diageye(ndims),
+    z_z_m_prior = zeros(ndims),
+    z_z_w_prior = 100.0*diageye(ndims),
+    z_w_transition_prior = 100.0,
+    y_w_transition_prior =  1/mnv,
+)
+    n_samples = length(obs)
+    marginals = Dict()
+    marginals[:A] = ProbabilityDistribution(ForneyLab.MatrixVariate, Dirichlet, a=ones(ndims, ndims))
+    marginals[:κ] = ProbabilityDistribution(ForneyLab.Multivariate, GaussianMeanPrecision, m = κ_m_prior, w = κ_w_prior)
+    marginals[:ω] = ProbabilityDistribution(ForneyLab.Multivariate, GaussianMeanPrecision, m = ω_m_prior, w = ω_w_prior)
+    marginals[pad(:z,1)] = vague(GaussianMeanPrecision)
+    marginals[pad(:x,1)] = vague(GaussianMeanPrecision)
+    marginals[pad(:s,1)] = vague(Categorical, ndims)
+    for t = 2:n_samples
+        marginals[pad(:z,t)] = ProbabilityDistribution(ForneyLab.Univariate, GaussianMeanPrecision, m = z_m_prior, w = z_w_prior)
+        marginals[pad(:x,t)] = ProbabilityDistribution(ForneyLab.Univariate, GaussianMeanPrecision, m = x_m_prior, w = x_w_prior)
+        marginals[pad(:s,t)] = ProbabilityDistribution(Categorical, p = ones(ndims) ./ ndims)
+        marginals[pad(:s,t)*:_*pad(:s,t-1)] = ProbabilityDistribution(Contingency,p=ones(ndims, ndims) ./ ndims)
+        marginals[pad(:z,t)*:_*pad(:z,t-1)] = ProbabilityDistribution(ForneyLab.Multivariate,GaussianMeanPrecision, m = z_z_m_prior, w = z_z_w_prior)
+        marginals[pad(:x,t)*:_*pad(:x,t-1)] = ProbabilityDistribution(ForneyLab.Multivariate,GaussianMeanPrecision, m = x_x_m_prior, w = x_x_w_prior)
+    end
+    data = Dict()
+    data[:y] = obs
+    data[:mz_prior1] = z_m_prior
+    data[:wz_prior1] = z_w_prior
+    data[:mx_prior1] = x_m_prior
+    data[:wx_prior1] = x_w_prior
+    data[:wy_prior1] = wy_prior1
+    for t = 1:n_samples
+        data[pad(:ωs, t)] = ω_m_prior
+        data[pad(:wz_transition, t)] = z_w_transition_prior
+        data[pad(:wy_transition, t)] = y_w_transition_prior
+    end
+
+
+    fe = Vector{Float64}(undef, n_its)
+
+    @showprogress "Iterations" for i = 1:n_its
+
+        stepX!(data, marginals)
+        stepS!(data, marginals)
+        stepA!(data, marginals)
+        stepZ!(data, marginals)
+
+        fe[i] = freeEnergy(data, marginals)
+    end
+
+    mz = [ForneyLab.unsafeMean(marginals[pad(:z,t)]) for t=1:n_samples]
+    vz = [ForneyLab.unsafeVar(marginals[pad(:z,t)]) for t=1:n_samples]
+    mx = [ForneyLab.unsafeMean(marginals[pad(:x,t)]) for t=1:n_samples]
+    vx = [ForneyLab.unsafeVar(marginals[pad(:x,t)]) for t=1:n_samples]
+    ms = [ForneyLab.unsafeMean(marginals[pad(:s,t)]) for t=1:n_samples]
+    return mz,vz,mx,vx,ms,fe
+end
+
+
+using CSV
+using DataFrames
+using Plots
+df = CSV.File("data/AAPL.csv") |> DataFrame
+plot(df[:Open])
+series = df[!, :Open]
+omegas = collect(-3:0)
+n_cats = length(omegas)
+code = generate_mp(n_cats, length(series))
+
+eval(Meta.parse(code))
+mz,vz,mx,vx,ms,fe = mp(series, ndims=n_cats, ω_m_prior=omegas, y_w_transition_prior=1/0.001)
